a48c94084b5133d5003547813d15988c606d73de
Noah <altenyt@gmail.com>
2020-05-15 19:17:37 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GDRPC.Sharp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
