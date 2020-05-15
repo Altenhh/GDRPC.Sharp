8662f903b08840a9355a3e0b236881ad690fa67c
Noah <altenyt@gmail.com>
2020-05-15 19:36:17 -0400

    Get GD Process

---
+++ Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GDRPC.Sharp
+{
+    public static class Helper
+    {
+        public static void Write(string message, ConsoleColor col = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(DateTime.Now.ToLongTimeString().PadRight(12));
+
+            Console.ForegroundColor = col;
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Binarysharp.MemoryManagement;
+using static GDRPC.Sharp.Helper;
 
 namespace GDRPC.Sharp
 {
     public static class Program
     {
+        private static Process gdProcess;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetGdProcess(args);
+            
+            var memory = new MemorySharp(gdProcess);
+        }
+
+        private static void GetGdProcess(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "--opengd")
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo(@"C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash\GeometryDash.exe")
+                {
+                    WorkingDirectory = @"C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash",
+                    UseShellExecute = false
+                };
+
+                gdProcess = Process.Start(processStartInfo);
+
+                // doesn't open immediately so we will have to wait a bit
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+
+                Write($"Started process: {gdProcess?.Id} (Geometry Dash)");
+            }
+            else
+            {
+                try
+                {
+                    gdProcess = Process.GetProcessesByName("GeometryDash")[0];
+                    Write($"Hooked onto process: {gdProcess.Id} ({gdProcess.MainWindowTitle})");
+                }
+                catch
+                {
+                    Write("Failed to hook onto process", ConsoleColor.Red);
+                }
+            }
         }
     }
 }
